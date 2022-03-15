4b742e5e53ea4e2cd62740b17d155a1c5990531c
calebapril <calebessien001@gmail.com>
2022-03-15 06:57:59 -0700

    3 commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _islower - checks for lowercase character
- * @c: the character to check
- * Return: 1 if c is lowercase, 0 otherwise
+ * _islower - checks for lowercase
+ * @c: c is an ascii character
+ *
+ * Return: 1 if lowercase
  */
+
 int _islower(int c)
 {
-	return (c >= 'a' && c <= 'z');
-}
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}}

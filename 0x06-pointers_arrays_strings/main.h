0021315d0d5b992592ef261f9b1747502558f8ac
calebapril <calebessien001@gmail.com>
2022-03-23 06:09:39 -0700

    Added main.h file

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#ifndef _main_H_
+#define _main_H_
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
+void print_number(int n);
+int _putchar(char);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
+#endif

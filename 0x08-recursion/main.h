b8ae1b3ccec2fa2074388742a4c77f620300dcea
calebapril <calebessien001@gmail.com>
2022-03-30 02:28:22 -0700

    added main.h & Readme file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+void _putchar(char c);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int _sqrt(int n, int i);
+int wildcmp(char *s1, char *s2);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+
+#endif

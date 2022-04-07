b0d2b1d37b409d36716bd84b81c602855c3a289f
calebapril <calebessien001@gmail.com>
2022-04-07 05:32:56 -0700

    added main.h and readme files

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,16 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+#endif

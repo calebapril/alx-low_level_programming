f19e456d464531fd6a0e41db4fede68527164431
calebapril <calebessien001@gmail.com>
2022-04-07 17:10:34 -0700

    last commit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 
+void _puts(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -11,6 +12,5 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
-void _puts(char *str);
 
 #endif

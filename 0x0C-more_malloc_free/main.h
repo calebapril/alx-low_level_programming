bc1c6ab8fda19c1e0493a7d69826184c45889518
calebapril <calebessien001@gmail.com>
2022-04-07 17:38:44 -0700

    last commit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,16 +1,17 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
-#include <stdlib.h>
 
-void _puts(char *str);
+#include <stdlib.h>
+#include <stdio.h>
+
+int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _putchar(char c);
-int _atoi(const char *s);
-void print_int(unsigned long int n);
-
 #endif

c65b0efcb740d5cf2a171a0ae0865e6f778e9d93
calebapril <calebessien001@gmail.com>
2022-06-13 14:09:10 -0700

    testing

---
+++ 0x18-dynamic_libraries/function.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * add - addition operator
+ * @a: first int
+ * @b: second int
+ * Return: sum of 2 numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtraction operator
+ * @a: first in
+ * @b: second int
+ * Return: difference of 2 numbers
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - product of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: product of the numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: result of the division of the numbers
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - get remainder of the division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: remainder of division of the numbers
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}

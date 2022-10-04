fdb274bac11ab9cca425967a204781b196d8ee8a
mulukenabay <106770288+mulukenabay@users.noreply.github.com>
2022-10-04 11:24:53 +0300

    Update 1-create_dynamic_lib.sh
    fix bug

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-gcc -c -fPIC *.c
+gcc -c -fPIC 0-isupper.c 0-strcat.c 1-isdigit.c 2-strchr.c 3-islower.c 3-strspn.c 5-strstr.c _putchar.c                       
+0-main.c 100-atoi.c 1-memcpy.c 2-strlen.c 3-puts.c 4-isalpha.c 6-abs.c                                     
+0-memset.c 100-operations.c 1-strncat.c 2-strncpy.c 3-strcmp.c 4-strpbrk.c 9-strcpy.c
 gcc -shared -o liball.so *.o
